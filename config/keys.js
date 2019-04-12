a58b5f6696b7196ac53996b4f22c5ccaf2e1a7cc
Jack Ma <jackma1206@gmail.com>
2019-04-12 16:21:32 -0400

    prod/dev setup

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleClientID: '842121781064-ubnodfrp0o2mqcackmihfs4r0s4c2iqh.apps.googleusercontent.com',
-    googleClientSecret: '2KSpnOYYzeBR_Cn5HHtLB1o-',
-    mongoURI: 'mongodb://jackma:jackma123@ds155651.mlab.com:55651/travelplanner-dev',
-    cookieKey: 'qwijdqiodjiqowjdioqwjdoiqwdjqowidjqwidjalskdjlkasdjlwda'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod');
+} else{
+    module.exports = require('./dev');
 }
