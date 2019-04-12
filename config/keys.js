6dfb5461a158c220ca67806da4c79454127755ba
Jack Ma <jackma1206@gmail.com>
2019-04-12 15:53:06 -0400

    added auth/login

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '842121781064-ubnodfrp0o2mqcackmihfs4r0s4c2iqh.apps.googleusercontent.com',
+    googleClientSecret: '2KSpnOYYzeBR_Cn5HHtLB1o-',
+    mongoURI: 'mongodb://jackma:jackma123@ds155651.mlab.com:55651/travelplanner-dev',
+    cookieKey: 'qwijdqiodjiqowjdioqwjdoiqwdjqowidjqwidjalskdjlkasdjlwda'
+}
